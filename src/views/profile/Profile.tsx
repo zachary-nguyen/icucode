d3eca187b3635c660ca2ed39d018c75f8f3ff5cb
Zac <zack.nguyen@hotmail.com>
2020-10-02 19:45:13 -0400

    remove unused imports

--- src/views/profile/Profile.tsx
+++ src/views/profile/Profile.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import ProfileDrawer from "../../components/login/ProfileDrawer";
+import ProfileDrawer from "../../components/profile/ProfileDrawer";
 import clsx from "clsx";
 import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
 import {Card, CardActionArea, Grid, Typography} from "@material-ui/core";
-import List from "@material-ui/core/List";
 
 const drawerWidth = 240;
 
