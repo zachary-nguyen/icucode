bbc21cd64fc8c06b5a9d3d716111ed216f450b6a
Zac <zack.nguyen@hotmail.com>
2020-10-02 19:44:51 -0400

    remove unused imports

--- src/components/login/ProfileDrawer.tsx
+++ src/components/login/ProfileDrawer.tsx
@@ -15,8 +15,6 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import SchoolIcon from '@material-ui/icons/School';
 import ClassIcon from '@material-ui/icons/Class';
 
