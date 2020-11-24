bf526c635857fe1f36188d93be49cff9ffb9f522
Zac <zack.nguyen@hotmail.com>
2020-11-23 21:50:58 -0500

    - unused imports

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import {MuiThemeProvider} from "@material-ui/core";
 
 ReactDOM.render(
     <BrowserRouter>
